8f7e1cad9843355e0f146dc51ea2c26bc8c2b5e3
Mikhail <31190223+mikhaildruzhinin@users.noreply.github.com>
2020-01-24 17:03:58 +0300

    initial upload

---
+++ main.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import requests
+import argparse
+from dotenv import load_dotenv
+import os
+
+def count_clicks(token, bitlink):
+	url_template = 'https://api-ssl.bitly.com/v4/bitlinks/{}/clicks/summary'
+	url = url_template.format(bitlink)
+	payload = {'unit':'day', 'units': -1}
+	headers={'Authorization': token}
+	response = requests.get(url, headers=headers, params=payload)
+	response.raise_for_status()
+	return response.json()['total_clicks']
+
+def shorten_link(token, link):
+	url = 'https://api-ssl.bitly.com/v4/shorten'
+	headers = {'Authorization': token}
+	payload = {'long_url': link}
+	response = requests.post(url, headers=headers, json=payload)
+	response.raise_for_status()
+	return response.json()['link']
+
+def main():
+
+	load_dotenv()
+
+	parser = argparse.ArgumentParser(description='Сокращённые ссылки')
+	parser.add_argument('url', type=str, help='Исходная ссылка')
+
+	ARGS = parser.parse_args()
+
+	token = os.getenv('BITLY_TOKEN')
+
+	try:
+		if ARGS.url.startswith('bit.ly'):
+			clicks_count = count_clicks(token, ARGS.url)
+		else:
+			bitlink = shorten_link(token, ARGS.url)
+	except requests.exceptions.HTTPError as e:
+		clicks_count = None
+		bitlink = None
+		print(e)
+	if ARGS.url.startswith('bit.ly') and clicks_count:
+		print('Всего кликов:', clicks_count)
+	elif not ARGS.url.startswith('bit.ly') and bitlink:
+			print('Битлинк', bitlink)
+
+if __name__ == '__main__':
+	main()
